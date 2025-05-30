2d8e6e8de877a19a52e0f17bd9df61dde8d538b9
AMAI-nayotake <194278002+AMAI>
2025-05-30 17:26:32 +0000

    [Update] usersコントローラ更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,23 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    
+    @books = @user.book.page(params[:page])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to edit_user_path(user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
   
 end
